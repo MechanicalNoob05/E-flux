798008c1f508a78c7cb623c2f38cbfd9cc4a9c65
tejas <tejasmayekar08@gmail.com>
2023-03-22 20:09:33 +0530

    Revert "login signup"
    This reverts commit 048b318ad08e149c20ae50c1834639fdde262beb.

--- Screens/Stack Screens/Signup Page/Signup.js
+++ Screens/Stack Screens/Signup Page/Signup.js
@@ -1,85 +1,12 @@
 import React from 'react'
-import { ApplicationProvider, Layout, Text, Input, Divider,Button } from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
-import { StyleSheet,  } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, View, Text, Button} from 'react-native'
 const Signup = (props) => {
-  const [value, setValue] = React.useState('');
-  const [pass, setPass] = React.useState('');
+
   const { navigation } = props
-
-  return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-          <ScrollView>
-
-      <Layout style={{ flex: 1, paddingTop: 45, padding: 10,  justifyContent:'center' , alignContent:'center'  }}>
-      <Text category='h3' style={styles.title}>Hello 👋 </Text>
-
-        <Text category='h2' style={{ marginBottom: 15, textAlign:'center'}}>
-          Create a New Account
-          </Text>
-
-          <Input style={{ padding: 10 }}
-          placeholder='John Doe'
-          
-          label='Name'
-        />
-        <Input style={{ padding: 10 }}
-          placeholder='demonslayer@emmail.com'
-          value={value}
-          label='Email'
-          onChangeText={nextValue => setValue(nextValue)}
-        />
-         <Input style={{ padding: 10 }}
-          placeholder='Tesla 3s'
-          
-          label='Car Model'
-        />
-         <Input style={{ padding: 10 }}
-          placeholder='eg. MHCET106'
-          caption='this will help us to create more customizations for you'
-          label='Car Number'
-        />
-        <Input style={{ padding: 10 }}
-          type='password'
-          placeholder='***********'
-          value={pass}
-          label='Password'
-          onChangeText={nextValue => setPass(nextValue)}
-        />
-        <Button style={styles.installButton} title='sign Up'>Sign Up</Button>
-        <Divider />
-      
-          <Text style={styles.signuptext}>
-           Already have an Account? Please <Text status='primary'
-              onPress={() => navigation.navigate('Login')}>Sign In</Text>
-          </Text>
-      </Layout>
-</ScrollView>
-    </ApplicationProvider>
-
-  )
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Signup Screen</Text>
+        </View>)
 }
-
-const styles = StyleSheet.create({
-  signuptext:{
-    flex: 1, alignContent:'center',
-    marginTop: 15,
-    textAlign: 'center'
-  },
-  details: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 4,
-  },
-  title: {
-    marginBottom: 45,
-    marginHorizontal: 8,
-    textAlign:'center'
-  },
-  installButton: {
-    marginVertical: 4,
-    marginBottom: 10
-  },
-});
+const styles = StyleSheet.create({ container: { padding: 24 } })
 export default Signup
