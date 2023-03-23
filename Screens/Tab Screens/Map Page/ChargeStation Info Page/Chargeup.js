42c4f929fe22bd73580bf462a5013780470679ab
tejas <tejasmayekar08@gmail.com>
2023-03-23 05:58:13 +0530

    oye hoy  button map navigation agay ahai bruahhhh

--- Screens/Tab Screens/Map Page/ChargeStation Info Page/Chargeup.js
+++ Screens/Tab Screens/Map Page/ChargeStation Info Page/Chargeup.js
@@ -1,32 +1,35 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button} from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
+import openMap from 'react-native-open-maps';
 
 const Chargeup = (props) => {
+  const map = () => {
 
-  const { navigation } = props
-    return (
-        <View style={styles.container}>
-            {/* <Text style={styles.text}>Chargeup Screen</Text>
+    openMap(
+      {
+        start: 'Plot No - 35, opp. Ganesh Mandir, Sector-5, Kharghar, Navi Mumbai, Maharashtra 410210',
+        end: "Plot No. 46, near MSEB Sub Station, Sector-5, Kharghar, Navi Mumbai, Maharashtra 410210",
+        travelType: 'drive',
+        navigate: true
+      }
+    )
+  }
+  return (
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>Chargeup Screen</Text>
             
             <Button title='='
                 onPress={() => navigation.navigate('M_Display')} /> */}
-                 <View style={styles.page}>
-      <View style={styles.container}>
-      </View>
-    </View>
-        </View>)
+      <Button onPress={map} title='Tu dabake to dekh isko'></Button>
+
+    </View>)
 }
-const styles = StyleSheet.create({ container: { padding: 24 },   page: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-container: {
-  height: 300,
-  width: 300,
-},
-map: {
-  flex: 1
-}
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    justifyContent: 'center',
+    height: '100%'
+  }
+
 })
 export default Chargeup
