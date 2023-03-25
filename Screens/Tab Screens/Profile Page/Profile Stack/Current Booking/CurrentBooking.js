b7f67615c774be497024275ec7f718c0a7544d2b
mayurjadhav2002 <mayurshrikantjadhav@gmail.com>
2023-03-25 17:34:22 +0530

    currnt booking card

--- Screens/Tab Screens/Profile Page/Profile Stack/Current Booking/CurrentBooking.js
+++ Screens/Tab Screens/Profile Page/Profile Stack/Current Booking/CurrentBooking.js
@@ -1,3 +1,4 @@
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import React from 'react'
 import { StyleSheet, View, Button} from 'react-native'
 import { Text } from 'react-native-elements'
@@ -7,8 +8,23 @@ const CurrentBooking = (props) => {
     return (
         <View style={styles.container}>
            <Text>See your Upcoming Bookings</Text>
-            
+            <View style={styles.card}>
+            <Text h4 style={{padding: 5}}>
+              Manish Garage Electric Charging Station
+            </Text>
+           
+              <Text> <Ionicons name='calendar-outline'/> 15 March</Text>
+              <Text> <Ionicons name='time'/> 12-1.0 PM</Text>
+              <Button title='View Location on Maps'></Button>
+            </View>
         </View>)
 }
-const styles = StyleSheet.create({ container: { padding: 24 } })
+const styles = StyleSheet.create({ container: { padding: 24 },
+ 
+card:{
+  padding: 10,
+  backgroundColor:'#fff',
+  borderRadius: 15
+}
+})
 export default CurrentBooking
