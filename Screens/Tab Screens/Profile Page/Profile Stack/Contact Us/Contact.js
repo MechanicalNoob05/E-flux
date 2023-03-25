6ba1d4984e7197ae982b2d2203cd1fe28de2a77d
mayurjadhav2002 <mayurshrikantjadhav@gmail.com>
2023-03-25 20:22:39 +0530

    new commit

--- Screens/Tab Screens/Profile Page/Profile Stack/Contact Us/Contact.js
+++ Screens/Tab Screens/Profile Page/Profile Stack/Contact Us/Contact.js
@@ -1,13 +1,85 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button} from 'react-native'
+import { ScrollView } from 'react-native'
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native'
+import { Divider } from 'react-native-elements'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 const Contact = (props) => {
 
-  const { navigation } = props
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Map_Display Screen</Text>
-            
-        </View>)
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+      <Text style={{fontSize: 20, textAlign:'center'}}>Contact Us</Text>
+
+      <View style={styles.inputview}>
+        <Text for="fname">Your Name</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+        />
+      </View>
+
+      <View style={styles.inputview}>
+        <Text for="fname">Email Address</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+        />
+      </View>
+
+      <View style={styles.inputview}>
+        <Text for="fname">Comments</Text>
+        <TextInput
+        multiline
+        numberOfLines={4}
+
+          style={styles.input2}
+        />
+      </View>
+      <View style={{ marginTop: 20, height: 50 }}>
+        <Button
+          style={{ height: 50,}}
+          title="Send"
+        />
+      </View>
+      <Divider/>
+<View style={{padding: 10,  textAlign: 'center'}}>
+  <Text style={{fontSize: 16, textAlign: 'center'}}><Ionicons name='mail-outline' style={{fontSize: 18, color: 'blue'}} /> customercare@eflux.com </Text>
+  <Text style={{fontSize: 16, textAlign: 'center'}}><Ionicons name='call-outline' style={{fontSize: 18, color: 'skyblue'}} /> +91 9163756591 </Text>
+  <Text style={{fontSize: 16, textAlign: 'center'}}><Ionicons name='location-outline' style={{fontSize: 18, color: 'red'}} /> Mumbai, India, 410206S </Text>
+
+</View>
+    </View></ScrollView>)
 }
-const styles = StyleSheet.create({ container: { padding: 24 } })
+const styles = StyleSheet.create(
+  {
+    container: {
+      marginTop:'10%',
+      padding: 24,
+
+    },
+    inputview: {
+      marginTop: 10,
+    },
+
+    input: {
+      height: 40,
+      borderWidth: 1,
+      borderColor: '#f8f8f8',
+      backgroundColor: '#e8e8e8',
+
+      padding: 10,
+      borderRadius: 5
+    },
+    input2: {
+      height: 80,
+      borderWidth: 1,
+      borderColor: '#f8f8f8',
+      backgroundColor: '#e8e8e8',
+
+      padding: 10,
+      borderRadius: 5
+    },
+  }
+)
 export default Contact
--- Screens/Tab Screens/Profile Page/Profile Stack/Help/Help.js
+++ Screens/Tab Screens/Profile Page/Profile Stack/Help/Help.js
@@ -4,10 +4,10 @@ const Help = (props) => {
 
   const { navigation } = props
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Map_Display Screen</Text>
-            
-        </View>)
+      <View style={styles.container}>
+      <Text h4  style={{textAlign:'center'}}>Help Section</Text>
+      
+  </View>)
 }
 const styles = StyleSheet.create({ container: { padding: 24 } })
 export default Help
